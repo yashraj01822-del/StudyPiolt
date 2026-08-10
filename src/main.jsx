996bfd0858b2c5ec9772c545e0b3a2415e89f421
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-10 16:15:13 +0530

    Initialize React application with App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
+function App() {
+  return (
+    <div className="app">
+      <h1>StudyPilot</h1>
+      <p>Your intelligent study companion.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
