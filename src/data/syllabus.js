8ffbff6cb84475f59b01d2e8f1e6e236f81e940b
Astrophotography With Yash <yashraj01822@gmail.com>
2026-08-10 19:29:16 +0530

    Add syllabus data for class 9 and class 10

--- src/data/syllabus.js
+++ src/data/syllabus.js
@@ -1,1 +1,23 @@
+const syllabus = {
+  class9: {
+    syllabus: "new",
+    subjects: {
+      english: [],
+      mathematics: [],
+      science: [],
+      socialScience: [],
+    },
+  },
 
+  class10: {
+    syllabus: "old",
+    subjects: {
+      english: [],
+      mathematics: [],
+      science: [],
+      socialScience: [],
+    },
+  },
+};
+
+export default syllabus;
